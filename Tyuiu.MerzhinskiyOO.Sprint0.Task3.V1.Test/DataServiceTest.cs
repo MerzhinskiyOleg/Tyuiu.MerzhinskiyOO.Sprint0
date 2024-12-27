0f774c4419774aae7dfb5519aa95126701fbf13f
Tyuiu.MerzhinskiyOO@outlook.com <Tyuiu.MerzhinskiyOO@outlook.com>
2024-12-27 22:52:16 +0500

    Task3

---
+++ Tyuiu.MerzhinskiyOO.Sprint0.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MerzhinskiyOO.Sprint0.Task3.V1.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint0.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.MerzhinskiyOO.Sprint0.Task3.V1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MerzhinskiyOO.Sprint0.Task3.V1.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint0.Task3.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();  
+        }
+    }
+}
