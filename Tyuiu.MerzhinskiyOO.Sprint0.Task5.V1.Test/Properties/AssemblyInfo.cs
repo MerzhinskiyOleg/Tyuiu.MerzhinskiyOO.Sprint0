a82333622ec5d6da9c8c3d8d45b37ce733a340e6
Tyuiu.MerzhinskiyOO@outlook.com <Tyuiu.MerzhinskiyOO@outlook.com>
2024-12-27 23:56:01 +0500

    Task5

---
+++ Tyuiu.MerzhinskiyOO.Sprint0.Task5.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MerzhinskiyOO.Sprint0.Task5.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MerzhinskiyOO.Sprint0.Task5.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8420d4ff-7e53-43d3-adf7-8a1f83288d6b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
