d24f503b6086902ad75e23c1e4261ead3110c136
Tyuiu.MerzhinskiyOO@outlook.com <Tyuiu.MerzhinskiyOO@outlook.com>
2024-12-27 21:59:32 +0500

    Task1

---
+++ Tyuiu.MerzhinskiyOO.Sprint0.Task1.V1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint0.Task1.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Олег";
+            int age = 21;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + "  ");
+            Console .ReadKey();
+           
+        }
+    }
+}
